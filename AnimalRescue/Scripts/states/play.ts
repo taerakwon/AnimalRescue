93db1ccc0bf9cd8dcd03314882930ac9aad553ae
taerakwon <taerakwon@gmail.com>
2015-08-05 13:00:31 -0400

    Updated button atlas and added tower buttons

--- AnimalRescue/Scripts/states/play.ts
+++ AnimalRescue/Scripts/states/play.ts
@@ -8,6 +8,11 @@ module state {
         private moneyLabel: objects.Label;
         private level1Grass: createjs.Bitmap;
 
+        private fireTower: objects.Button;
+        private icetower: objects.Button;
+        private rocktower: objects.Button;
+        private goldtower: objects.Button;
+
         constructor() {
             this._Main();
         }
@@ -32,11 +37,32 @@ module state {
             // Add Scoreboard
             scoreBoard = new objects.ScoreBoard();
 
-            // Adds Grass
+            // Add Grass
             this.level1Grass = new createjs.Bitmap(assets.loader.getResult("level1Grass"));
             this.level1Grass.x = 700;
             this.level1Grass.y = 100;
             game.addChild(this.level1Grass);            
+
+            // Add Tower Buttons
+            this.fireTower = new objects.Button("firetower", "firetowerDark");
+            this.fireTower.x = 60;
+            this.fireTower.y = 510;
+            game.addChild(this.fireTower);
+
+            this.icetower = new objects.Button("icetower", "icetowerDark");
+            this.icetower.x = 160;
+            this.icetower.y = 510;
+            game.addChild(this.icetower);
+
+            this.rocktower = new objects.Button("rocktower", "rocktowerDark");
+            this.rocktower.x = 260;
+            this.rocktower.y = 510;
+            game.addChild(this.rocktower);
+
+            this.goldtower = new objects.Button("goldtower", "goldtowerDark");
+            this.goldtower.x = 360;
+            this.goldtower.y = 510;
+            game.addChild(this.goldtower);
 
             // Add game container to stage
             stage.addChild(game);
